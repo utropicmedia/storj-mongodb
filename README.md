c636022b32bb5520ad7fda4adb3257d063e933f2
Satyam Shivam <satyam.shivam@neosofttech.com>
2019-07-05 12:56:30 +0530

    SC-4: part 1 - command line option to 'connect' with mongo db instance

--- README.md
+++ README.md
@@ -1,19 +1,52 @@
 # storj-mongodb
 
-# Initial Set-up
-* Create a folder "github.com" within $GOPATH/src
-* Create a folder "urfave" within $GOPATH/src/github.com
-* Clone "github.com/urfave/cli" within $GOPATH/src/github.com/urfave
+## Initial Set-up
+Make sure your `PATH` includes the `$GOPATH/bin` directory so your commands can be easily used:
+```
+export PATH=$PATH:$GOPATH/bin
+```
 
-# Build ONCE
+Install [github.com/urfave/cli](https://github.com/urfave/cli), by running:
+```
+$ go get github.com/urfave/cli
+```
+
+Install mongo-driver go package, by running:
+```
+$ go get go.mongodb.org/mongo-driver/mongo
+```
+
+## Build ONCE
 ```
 $ go build storj_mongodb.go
 ```
 
-# Run the command-line tool
+## Set-up Files
+* Create a `db_property` file, with following contents about a MongoDB instance:
+    Host Name
+    Port Number
+    User Name
+    Password
+* Create a `storj_config.json` file, with Storj network's configuration information in JSON format
+* Store both these files in a `config` folder
+
+## Run the command-line tool
+* Get help
 ```
 $ ./storj_mongodb.go -h
+```
+* Check version
+```
 $ ./storj_mongodb.go -v
-$ ./storj_mongodb.go d
-$ ./storj_mongodb.go s
+```
+* Read MongoDB instance property from a desired file
+```
+$ ./storj_mongodb.go d ./config/db_property
+```
+* Read MongoDB instance property from a desired file
+```
+$ ./storj_mongodb.go c ./config/db_property
+* Read and parse Storj network's configuration, in JSON format, from a desired file
+```
+$ ./storj_mongodb.go s ./config/storj_config.json
 ```
--- storj_mongodb.go
+++ storj_mongodb.go
@@ -1,38 +1,83 @@
 /*
  * package to read MongoDB collections into a Storj bucket
  *
+ * v 1.0.1
+ * provides option to connect to a MongoDB instance,
+ * whose properties are read from given file
+ *
  * v 1.0.0
  * provides options to read following files:
  * 	- MongoDB property
- *	- Storj Configuration information in  JSON format
+ *	- Storj Configuration information in JSON format
  */
 
 package main
 
 import (
-    "fmt"
-    "os"
+    "bufio"
+    "context"
     "encoding/json"
+    "fmt"
     "github.com/urfave/cli"
 	"log"
-	"bufio"
+	"os"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// readPropertiesDB reads the database property file
-// and returns all the credentials information
-func readPropertiesDB(fullFileName string) ([]string, error) {
-	fileHandle, err := os.Open(fullFileName)
-	defer fileHandle.Close()
-	if err != nil {
-		return nil, err
-	}
+// ConfigMongoDB define variables and types
+type ConfigMongoDB struct {
+
+	fullFileName	string
+	//
+	hostName 		string
+	portNumber		string
+	userName 		string 
+	password		string
+
+}
+
+var gO_configMongoDB ConfigMongoDB
+
+// readPropertiesDB asks for the name of a file
+// wherefrom it reads the database property contents
+// and returns all the credentials information as an object
+func readPropertiesDB(cliContext *cli.Context) (ConfigMongoDB, error) {
+	var configMongoDB ConfigMongoDB
 	
-	var properties []string
+	// default full file name
+	configMongoDB.fullFileName = "./config/db_property"
+
+	if len(cliContext.Args()) > 0 {
+		configMongoDB.fullFileName = cliContext.Args()[0]
+	}
+
+	// open the desired file
+	fileHandle, err := os.Open(configMongoDB.fullFileName)
+	if err != nil {
+		return configMongoDB, err
+	}
+	defer fileHandle.Close()
+	
+	// read contents of the file
+	var dbProperty []string
 	scanner := bufio.NewScanner(fileHandle)
 	for scanner.Scan() {
-		properties = append(properties, scanner.Text())
+		dbProperty = append(dbProperty, scanner.Text())
 	}
-	return properties, scanner.Err()
+	//
+	err = scanner.Err()
+	if err != nil {
+		return configMongoDB, err
+	}
+
+	// store contents into the structure elements 
+	configMongoDB.hostName 	 = dbProperty[0]
+	configMongoDB.portNumber = dbProperty[1]
+	configMongoDB.userName   = dbProperty[2]
+	configMongoDB.password 	 = dbProperty[3]
+	
+	return configMongoDB, nil
 }
 
 // ConfigStorj depicts keys to search for
@@ -50,11 +95,10 @@ func LoadStorjConfiguration(fullFileName string) (ConfigStorj, error) {
 	var configStorj ConfigStorj
 	
 	fileHandle, err := os.Open(fullFileName)
-	defer fileHandle.Close()
 	if err != nil {
 		return configStorj, err
-		// fmt.Println(err.Error())
 	}
+	defer fileHandle.Close()
 	
 	jsonParser := json.NewDecoder(fileHandle)
 	jsonParser.Decode(&configStorj)
@@ -69,7 +113,7 @@ func setAppInfo() {
 	app.Name = "Storj MongoDB Connector"
 	app.Usage = "Backup your MongoDB collections to decentralized Storj cloud"
 	app.Author = "Satyam Shivam" 
-	app.Version = "1.0.0"
+	app.Version = "1.0.1"
 }
 
 func setCommands() {
@@ -79,39 +123,33 @@ func setCommands() {
 			Name:    "mongodb.props",
 			Aliases: []string{"d"},
 			Usage:   "use this command to read properties of desired mongo database\n\n     arguments-\n\n       1. fileName [optional] = provide full file name (with complete path), storing mongoDB credentials\n\n       if this filename is not given, then data is read from ./config/db_connector\n\n     example =   ./storj_mongodb d ./config/db_property\n\n\n",
-			Action: func(c *cli.Context) { 
+			Action: func(cliContext *cli.Context) { 
 
-				// default full file name
-				var fullFileName string="./config/db_property"
-
-				if len(c.Args())>0 {
-					fullFileName = c.Args()[0]
-				}
-
-				mongoDbProperty, err := readPropertiesDB(fullFileName)
+				gO_configMongoDB, err := readPropertiesDB(cliContext)
+				//
 				if err != nil {
 					log.Fatalf("readPropertiesDB: %s", err)
 				}
 				
 				// print to debug
-				fmt.Println("Read MongoDB credentials from the ", fullFileName, " file")
-				fmt.Println("Host Name	: ", mongoDbProperty[0])
-				fmt.Println("Port Number\t: ", mongoDbProperty[1])
-				fmt.Println("User Name	: ", mongoDbProperty[2])
-				fmt.Println("Password	: ", mongoDbProperty[3])
+				fmt.Println("Read MongoDB credentials from the ", gO_configMongoDB.fullFileName, " file")
+				fmt.Println("Host Name	: ", gO_configMongoDB.hostName)
+				fmt.Println("Port Number\t: ", gO_configMongoDB.portNumber)
+				fmt.Println("User Name	: ", gO_configMongoDB.userName)
+				fmt.Println("Password	: ", gO_configMongoDB.password)
 			},
 		},
 		{
 			Name:    "storj.config",
 			Aliases: []string{"s"},
 			Usage:   "use this command to read and parse JSON information about Storj network\n\n     arguments-\n\n       1. fileName [optional] = provide full file name (with complete path), storing Storj configuration information\n\n       if this filename is not given, then data is read from ./config/storj_config.json\n\n     example =   ./storj_mongodb s ./config/storj_config.json\n\n\n",
-			Action: func(c *cli.Context) { 
+			Action: func(cliContext *cli.Context) { 
 
 				// default full file name
 				var fullFileName string="./config/storj_config.json"
 
-				if len(c.Args())>0 {
-					fullFileName = c.Args()[0]
+				if len(cliContext.Args()) > 0 {
+					fullFileName = cliContext.Args()[0]
 				}
 
 				configStorj, err := LoadStorjConfiguration(fullFileName)
@@ -125,6 +163,45 @@ func setCommands() {
 				fmt.Println("Satellite	: ", configStorj.Satellite)
 				fmt.Println("Bucket		: ", configStorj.Bucket)
 				fmt.Println("Upload Path\t: ", configStorj.UploadPath)
+			},
+		},
+		{
+			Name : "connect",
+			Aliases : []string{"c"},
+			Usage : "use this command to connect with a mongoDB instance\n\n     arguments-\n\n       1. fileName [optional] = provide full file name (with complete path), storing mongoDB credentials\n\n       if this filename is not given, then data is read from ./config/db_property\n\n     example =   ./storj_mongodb c ./config/db_property\n\n\n",
+			Action : func(cliContext *cli.Context){
+				// read from a file
+				var err error
+				gO_configMongoDB, err = readPropertiesDB(cliContext)
+				if err != nil {
+					log.Fatalf("readPropertiesDB: %s", err)
+				}
+				
+				// print to debug
+				fmt.Println("Read MongoDB credentials from the ", gO_configMongoDB.fullFileName, " file")
+				fmt.Println("Host Name	: ", gO_configMongoDB.hostName)
+				fmt.Println("Port Number\t: ", gO_configMongoDB.portNumber)
+				fmt.Println("User Name	: ", gO_configMongoDB.userName)
+				fmt.Println("Password	: ", gO_configMongoDB.password)
+
+
+				// connection to mongoDB
+				mongoURL := fmt.Sprintf("mongodb://%s:%s@%s:%s/admin?authSource=admin", gO_configMongoDB.userName, gO_configMongoDB.password, gO_configMongoDB.hostName, gO_configMongoDB.portNumber)
+				clientOptions := options.Client().ApplyURI(mongoURL)
+				client, err := mongo.Connect(context.TODO(), clientOptions)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				// Check the connection
+				err = client.Ping(context.TODO(), nil)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				// print if connection successfully
+				fmt.Println("Connected to MongoDB!")
 			},
 		},
 	}
